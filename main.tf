3b800c20cfe201507387606e39df7c1088975649
rreddyga <ramanag2299@gmail.com>
2026-03-10 14:09:40 +0530

    vpc

--- main.tf
+++ main.tf
@@ -2,19 +2,10 @@
 # create internetgateway and attach to vpc
 
 resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
-  enable_dns_hostname = true
-
- # tags = {
- #   Name = "main"
- # }
-
-  tags = merge(
-    local.common_tags,
-    {
-        Name = "${var.project}- ${var.env}"
-    }
-    var.vpc_tags
-  )
+  cidr_block           = "10.0.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_support   = true    # Add this - enables DNS within VPC
+  enable_dns_hostnames = true    # Fixed spacing for consistency
+  
+  tags = local.vpc_final_tags
 }
