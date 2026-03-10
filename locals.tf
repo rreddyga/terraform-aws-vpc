7e1e63f3efcc1afa400f80443497ba37c2d54ff6
rreddyga <ramanag2299@gmail.com>
2026-03-10 14:21:17 +0530

    vpc

--- locals.tf
+++ locals.tf
@@ -7,7 +7,7 @@ locals {
     vpc_final_tags = merge(
         local.common_tags,
         {
-            Name = "${var.project}-${var.environment}"
+            Name = "${var.project}-${var.env}"
         },
         var.vpc_tags
     )
