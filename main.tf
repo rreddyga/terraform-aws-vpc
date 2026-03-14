f6317a1a397371061a7611023450df78ebaf05db
rreddyga <ramanag2299@gmail.com>
2026-03-14 19:56:16 +0530

    updated

--- main.tf
+++ main.tf
@@ -186,6 +186,27 @@ resource "aws_route" "database" {
   destination_cidr_block    = "0.0.0.0/0"
   #gateway_id = aws.aws_internet_gateway.main.id
   nat_gateway_id = aws_nat_gateway.nat.id
-
 }
+
+#public route_table association
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnets_cidr)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+#private route_table association
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnets_cidr)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
+#database route_table association
+resource "aws_route_table_association" "database" {
+  count = length(var.database_subnets_cidr)
+  subnet_id      = aws_subnet.database[count.index].id
+  route_table_id = aws_route_table.database.id
+}
+
 
