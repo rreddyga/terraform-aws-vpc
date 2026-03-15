28460c040331d03133fa37b321f9c171a5ab59d5
rreddyga <ramanag2299@gmail.com>
2026-03-15 12:17:50 +0530

    vpc peering

--- peering.tf
+++ peering.tf
@@ -46,6 +46,6 @@ resource "aws_route" "database_peering" {
 resource "aws_route" "default_peering" {
   count = var.is_vpc_peering_required ? 1 : 0
   route_table_id            = data.aws_route_table.main.id
-  destination_cidr_block    = var.cidr_block
+  destination_cidr_block    = var.vpc_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
 }
