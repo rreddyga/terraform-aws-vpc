02b35f32cc1ff5185aa88a0d44fefde9d0897714
rreddyga <ramanag2299@gmail.com>
2026-03-19 17:14:10 +0530

    subnet_ids added

--- output.tf
+++ output.tf
@@ -4,3 +4,18 @@ output "az_info" {
 output "vpc_id" {
     value = aws_vpc.main.id
 }
+
+#public subet_id we have to store ssm paramters
+output "public_subnet_ids" {
+    value = aws_subnet.public[*].id
+}
+
+#private subet_id we have to store ssm paramters
+output "private_subnet_ids" {
+    value = aws_subnet.private[*].id
+}
+
+#database subet_id we have to store ssm paramters
+output "database_subnet_ids" {
+    value = aws_subnet.database[*].id
+}
